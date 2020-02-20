4859903ad7768b5922570215dcdfdf10f46fbfa4
ex-force <ex-force-tfg@outlook.com>
2020-02-20 07:57:27 -0300

    Identity

--- ProjetoCasaDeShow/Controllers/CasaDeShowController.cs
+++ ProjetoCasaDeShow/Controllers/CasaDeShowController.cs
@@ -19,6 +19,8 @@ namespace ProjetoCasaDeShow.Controllers
             return View(casa);
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
         public IActionResult AddCasa(CasaDeShow casaDeShow, IFormFile arquivo){
             if(casaDeShow.Id == 0){
                 dataService.GetCasaDeShowRepository().Add(casaDeShow);
